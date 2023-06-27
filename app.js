a1d29907da90b2c4a1cc67d09be3a2235f4fa8d5
Sravanthi <sravathi@Manohars-Air.lan>
2023-06-27 11:23:39 -0400

    episode 01

---
+++ app.js
@@ -0,0 +1,5 @@
+
+
+const heading = React.createElement("h1", { id: "heading" }, "namste react from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
