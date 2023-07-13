7422fe07dddb079f95fa05230a0f9a65000cfedb
Sravanthi <sravathi@Manohars-Air.lan>
2023-07-13 15:57:48 -0400

    episode 1 and 2

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-const heading = React.createElement("h1", { id: "heading" }, "namste react from react");
+// const heading = <h1 id="heading"> {"namste react from freact"} </h1>;
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "namste react from freact"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(heading);
